8290ca5e7ef3e54b8c998bc5be0732d0b4fac9e5
Nguyễn Thành Minh Hiến <minhhien2710@users.noreply.github.com>
2019-11-08 00:14:05 +0700

    Chức năng đăng nhập + chứng thực

---
+++ ALTP_LARAVEL/database/seeds/ThemQuanTriVienSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ThemQuanTriVienSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\QuanTriVien::create([
+            'ten_dang_nhap' => 'admin',
+            'mat_khau'      => Hash::make('3hteam'),
+            'ho_ten'        => 'Quan Tri Vien'
+        ]);
+    }
+}
--- ALTP_LARAVEL/routes/web.php
+++ ALTP_LARAVEL/routes/web.php
@@ -11,36 +11,44 @@
 |
 */
 
-Route::get('/', function () {
-    return view('layout');
-})->name('dashboard');
+//Đăng nhập
+Route::get('dang-nhap', 'QuanTriVienController@dangNhap')->name('dang-nhap');
+Route::post('dang-nhap', 'QuanTriVienController@xuLyDangNhap')->name('xu-ly-dang-nhap');
+//Đăng xuất
+Route::get('dang-xuat', 'QuanTriVienController@dangXuat')->name('dang-xuat');
 
-Route::prefix('linh-vuc')->group(function(){
-    Route::name('linh-vuc.')->group(function(){
-        //Danh sách lĩnh vực
-        Route::get('/', 'LinhVucController@index')->name('danh-sach');
-        //Thêm mới lĩnh vực
-        Route::get('/them-moi', 'LinhVucController@create')->name('them-moi');
-        Route::post('/them-moi', 'LinhVucController@store')->name('xu-ly-them-moi');
-        //Cập nhập lĩnh vực
-        Route::get('/cap-nhap/{id}', 'LinhVucController@edit')->name('cap-nhap');
-        Route::post('/cap-nhap/{id}', 'LinhVucController@update')->name('xu-ly-cap-nhap');
-        //Xoá lĩnh vực
-        Route::get('/xoa/{id}', 'LinhVucController@destroy')->name('xoa');
+Route::middleware('auth')->group(function(){
+    Route::get('/', function () {
+        return view('layout');
+    })->name('trang-chu');
+
+    Route::prefix('linh-vuc')->group(function(){
+        Route::name('linh-vuc.')->group(function(){
+            //Danh sách lĩnh vực
+            Route::get('/', 'LinhVucController@index')->name('danh-sach');
+            //Thêm mới lĩnh vực
+            Route::get('/them-moi', 'LinhVucController@create')->name('them-moi');
+            Route::post('/them-moi', 'LinhVucController@store')->name('xu-ly-them-moi');
+            //Cập nhập lĩnh vực
+            Route::get('/cap-nhap/{id}', 'LinhVucController@edit')->name('cap-nhap');
+            Route::post('/cap-nhap/{id}', 'LinhVucController@update')->name('xu-ly-cap-nhap');
+            //Xoá lĩnh vực
+            Route::get('/xoa/{id}', 'LinhVucController@destroy')->name('xoa');
+        });
     });
-});
 
-Route::prefix('cau-hoi')->group(function(){
-    Route::name('cau-hoi.')->group(function(){
-        //Danh sách lĩnh vực
-        Route::get('/', 'CauHoiController@index')->name('danh-sach');
-        //Thêm mới lĩnh vực
-        Route::get('/them-moi', 'CauHoiController@create')->name('them-moi');
-        Route::post('/them-moi', 'CauHoiController@store')->name('xu-ly-them-moi');
-        //Cập nhập lĩnh vực
-        Route::get('/cap-nhap/{id}', 'CauHoiController@edit')->name('cap-nhap');
-        Route::post('/cap-nhap/{id}', 'CauHoiController@update')->name('xu-ly-cap-nhap');
-        //Xoá lĩnh vực
-        Route::get('/xoa/{id}', 'CauHoiController@destroy')->name('xoa');
+    Route::prefix('cau-hoi')->group(function(){
+        Route::name('cau-hoi.')->group(function(){
+            //Danh sách Câu hỏi
+            Route::get('/', 'CauHoiController@index')->name('danh-sach');
+            //Thêm mới Câu hỏi
+            Route::get('/them-moi', 'CauHoiController@create')->name('them-moi');
+            Route::post('/them-moi', 'CauHoiController@store')->name('xu-ly-them-moi');
+            //Cập nhập Câu hỏi
+            Route::get('/cap-nhap/{id}', 'CauHoiController@edit')->name('cap-nhap');
+            Route::post('/cap-nhap/{id}', 'CauHoiController@update')->name('xu-ly-cap-nhap');
+            //Xoá Câu hỏi
+            Route::get('/xoa/{id}', 'CauHoiController@destroy')->name('xoa');
+        });
     });
 });
