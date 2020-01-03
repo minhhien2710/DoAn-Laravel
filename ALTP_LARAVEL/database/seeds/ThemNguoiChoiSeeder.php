887e8509946f6215c793f85761ee628486240741
Nguyễn Thành Minh Hiến <minhhien2710@users.noreply.github.com>
2020-01-04 04:21:50 +0700

    ...

--- ALTP_LARAVEL/database/seeds/ThemNguoiChoiSeeder.php
+++ ALTP_LARAVEL/database/seeds/ThemNguoiChoiSeeder.php
@@ -15,7 +15,7 @@ class ThemNguoiChoiSeeder extends Seeder
             'ten_dang_nhap' => 'user1',
             'mat_khau'      => Hash::make('123123'),
             'email'         => 'user1@gmail.com',
-            'anh_dai_dien'  => '',
+            'anh_dai_dien'  => '16-52-08_31-12-2019_avatar-03.jpg',
             'diem_cao_nhat' => 0,
             'credit'        => 0
         ]);
@@ -24,7 +24,7 @@ class ThemNguoiChoiSeeder extends Seeder
             'ten_dang_nhap' => 'user2',
             'mat_khau'      => Hash::make('123123'),
             'email'         => 'user2@gmail.com',
-            'anh_dai_dien'  => '',
+            'anh_dai_dien'  => 'avatar-05.jpg',
             'diem_cao_nhat' => 0,
             'credit'        => 0
         ]);
